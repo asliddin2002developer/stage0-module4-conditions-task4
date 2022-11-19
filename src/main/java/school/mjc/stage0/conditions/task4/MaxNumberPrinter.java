09f0facc518785f09d884aa9e3d23099ab6e2b68
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2022-11-19 06:26:07 +0500

    Ternary Assignment

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int maxNumber = first >= second ? first : second;
+        maxNumber = maxNumber >= third ? maxNumber : third;
     }
 }
